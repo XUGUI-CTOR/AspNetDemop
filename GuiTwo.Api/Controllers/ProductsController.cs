524975a32d26ed49c4ca095c83d4052d85429975
unknown <2957164533@qq.com>
2021-05-05 15:12:12 +0800

    初始化提交、

---
+++ GuiTwo.Api/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using GuiTwo.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GuiTwo.Api.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts() => products;
+
+        public IHttpActionResult GetProduct(int id) => Ok(products.SingleOrDefault(x => x.Id == id));
+    }
+}
---
+++ GuiTwo.Api/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace GuiTwo.Api.Models
+{
+    public class Product
+    {
+        [Description("ID")]
+        public int Id { get; set; }
+        [Description("产品名称")]
+        public string Name { get; set; }
+        [Description("产品类型")]
+        public string Category { get; set; }
+        [Description("产品价格")]
+        public decimal Price { get; set; }
+    }
+}
