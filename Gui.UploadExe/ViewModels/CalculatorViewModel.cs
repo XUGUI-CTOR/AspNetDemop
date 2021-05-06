7d4753caf4fcbf4c18342d5d9fab0f119b5408be
XUGUI-CTOR <55677287+XUGUI-CTOR@users.noreply.github.com>
2021-05-06 23:05:11 +0800

    202005062305

---
+++ Gui.UploadExe/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Caliburn.Micro;
+using HandyControl.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using MessageBox = HandyControl.Controls.MessageBox;
+
+namespace Gui.UploadExe.ViewModels
+{
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public class CalculatorViewModel : Screen
+    {
+        public double Left { get; set; }
+        public double Right { get; set; }
+        public double Result { get; set; }
+        public CalculatorViewModel()
+        {
+
+        }
+        public bool CanDivide(double left, double right)
+        {
+            return right != 0;
+        }
+
+        public void Divide(double left, double right)
+        {
+            Thread.Sleep(600);
+            if (CanDivide(left, right) == true)
+                Result = left / right;
+            else MessageBox.Show("Divider cannot be zero.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+
+        public void Plus(double left, double right)
+        {
+            Result = left + right;
+        }
+
+        public void Minus(double left, double right)
+        {
+            Result = left - right;
+        }
+
+        public void Multipy(double left, double right)
+        {
+            Result = left * right;
+        }
+    }
+}
---
+++ Gui.UploadExe/ViewModels/ShellViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gui.UploadExe.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        public ShellViewModel()
+        {
+
+        }
+        public async void ShowCalculator()
+        {
+           await ActivateItemAsync(new CalculatorViewModel());
+        }
+    }
+}
