a2a067c3ac98629f2768ad4b674a5e2e4f305f3f
XUGUI-CTOR <55677287+XUGUI-CTOR@users.noreply.github.com>
2021-05-13 22:43:32 +0800

    202105132243

--- ASP.NET.MVC.Demo/Models/Movie.cs
+++ ASP.NET.MVC.Demo/Models/Movie.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data.Entity;
 
 namespace ASP.NET.MVC.Demo.Models
 {
@@ -12,5 +10,10 @@ namespace ASP.NET.MVC.Demo.Models
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
     }
 }
